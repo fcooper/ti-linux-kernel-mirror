e063dd85090c09245fc966b304c13bec921fe28a
Nishanth Menon <nm@ti.com>
2014-05-13 22:33:54 -0500

    ARM: OMAP2+: DMA: remove requirement of irq for platform-dma driver
    we have currently 2 DMA drivers that try to co-exist.
    drivers/dma/omap-dma.c which registers it's own IRQ and is device tree
    aware and uses arch/arm/plat-omap/dma.c instance created by
    arch/arm/mach-omap2/dma.c to maintain channel usage (omap_request_dma).
    
    Currently both try to register interrupts and mach-omap2/plat-omap dma.c
    attempts to use the IRQ number registered by hwmod to register it's own
    interrupt handler.
    
    Now, there is no reasonable way of static allocating DMA irq in GIC
    SPI when we use crossbar. However, since the dma_chan structure is
    freed as a result of IRQ not being present due to devm allocation,
    maintaining information of channel by platform code fails at a later
    point in time when that region of memory is reused.
    
    So, if hwmod does not indicate an IRQ number, then, assume that
    dma-engine will take care of the interrupt handling.
    
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Acked-by: Sricharan R <r.sricharan@ti.com>

--- include/linux/omap-dma.h
+++ include/linux/omap-dma.h
@@ -147,6 +147,7 @@ static inline bool omap_dma_filter_fn(struct dma_chan *c, void *d)
 #define IS_WORD_16			BIT(0xd)
 #define ENABLE_16XX_MODE		BIT(0xe)
 #define HS_CHANNELS_RESERVED		BIT(0xf)
+#define DMA_ENGINE_HANDLE_IRQ		BIT(0x10)
 
 /* Defines for DMA Capabilities */
 #define DMA_HAS_TRANSPARENT_CAPS	(0x1 << 18)
